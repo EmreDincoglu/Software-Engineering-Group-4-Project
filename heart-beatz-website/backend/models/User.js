5ddb603c0cca6dfb7cce90342a1c35c0c7bb95ff
EmreDincoglu <Dincoglue@gmail.com>
2024-10-15 14:43:49 -0400

    No duplicate usernames or emails
    updated the createUser api to make it so duplicate emails and usernames cannot be put into the database, also sends back a json saying what is the duplicate

--- heart-beatz-website/backend/models/User.js
+++ heart-beatz-website/backend/models/User.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
-    email: { type: String, required: true }
+    email: { type: String, required: true },
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    age: { type: Number, required: true}
 });
 
 module.exports = mongoose. model('userAccount', userSchema);
--- heart-beatz-website/backend/mongoose.js
+++ heart-beatz-website/backend/mongoose.js
@@ -9,16 +9,52 @@ mongoose.connect('mongodb+srv://dincoglue:aT8C5J5D6Jw6wWfW@cluster0.e7oni.mongod
     console.log('Connection failed')
 })
 
+async function checkUsername(username) {
+    const users = mongoose.model('userAccount', userAccount.schema);
+    const user = await users.findOne({username: username});
+    if (user) {
+        return "Username is already in use.";
+    }
+    return null;
+}
+
+async function checkEmail(email) {
+    const users = mongoose.model('userAccount', userAccount.schema);
+    const user = await users.findOne({email: email});
+    if (user) {
+        return "Email is already in use.";
+    }
+    return null;
+}
+
 const createUser = async (req, res, next) => {
     
     const createdUser = new userAccount({
-        username: req.body.username,
+        username: req.body.username.toLowerCase(),
         password: req.body.password,
-        email: req.body.email
+        email: req.body.email.toLowerCase()
     });
-    console.log(createdUser.username, createdUser.password, createdUser.email);
-    
-    const result = await createdUser.save();
+
+    const dupeUsername = await checkUsername(createdUser.username);
+    const dupeEmail = await checkEmail(createdUser.email);
+    console.log(dupeUsername, dupeEmail);
+    let result;
+
+    if (dupeUsername === null && dupeEmail === null) {
+        console.log("reached")
+        result = await createdUser.save();
+    }
+    else {
+        if (dupeUsername && dupeEmail) {
+            result = "Username and Email in use.";
+        }
+        else if (dupeUsername) {
+            result = dupeUsername;
+        }
+        else {
+            result = dupeEmail;
+        }
+    }
     res.json(result);
 }
 
