f397c509c5eb07069332e97b2f21f1ea8e7a24bd
Jess Barnes <jessicabarnes@ufl.edu>
2024-11-05 17:26:42 -0500

    Adding photos and changed nav bar

--- frontend/src/pages/Root/root.js
+++ frontend/src/pages/Root/root.js
@@ -1,7 +1,14 @@
 import { Outlet, Link } from "react-router-dom";
 import './root.css';
+import React from 'react';
+import { useAuth } from '../Login/AuthProvider';
 // This defines global web page constants, such as the navbar
-export function RootPage(){return (
+
+
+export function RootPage(){
+    const { isLoggedIn } = useAuth();
+
+    return (
     <>
         <div className='nav-bar'>
             <div className='app-name'>
@@ -9,18 +16,43 @@ export function RootPage(){return (
                 <Link className="app-name-beatz" to="/">Beatz</Link>
             </div>
             <div className='nav-bar-page-links'>
-                <Link to="/home">Home</Link>
-                <Link to="/">About</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/profile">Profile</Link>
+                {isLoggedIn ? (
+                    <>
+                        <Link to="/home">Home</Link>
+                    <Link to="/profile">Profile</Link>
+                        </>
+                    ) : (
+                        <>
+                    <Link to="/login">Login</Link>
+                    <Link to="/">About</Link>
+                    </>
+                    )}
+                </div>
             </div>
-        </div>
-        <Outlet/>
-    </>
-);}
+            <Outlet/>
+        </>
+        );}
 
-export function AboutPage(){return (
-    <div>
-        <text>About Page</text>
+export function AboutPage(){
+    return (
+        <div className = "body">
+    <div className = "title">
+        <div className = "big-photo">
+        <img src = "/img/peoplelisteningtomusicholdinghands.jpg" alt = "People listening to music holding hands" />
+        </div>
+        <div className= "title-name">
+            <h1>HEARTBEATZ</h1>
+        </div>
     </div>
-);}
+    <div className= "about-us">
+        <div className= "grid-background"/>
+            <div className= "small-photo">
+                <img src = "/img/peoplesharingearbuds.jpg" alt = "People sharing earbuds"/>
+            </div>
+            <div className= "about-us-text">
+                <h3> YADA YADA YADA</h3>
+            </div>
+     </div>
+   </div>
+    );
+}
